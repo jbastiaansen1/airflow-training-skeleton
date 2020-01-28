1a338522c27799ea3cbcdbf7b70db8e4fa3e8eed
jbastiaansen1 <60383797+jbastiaansen1@users.noreply.github.com>
2020-01-28 13:11:50 +0100

    Update test_dag.py

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -1,35 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
-
-
 from datetime import timedelta
-
+from datetime import datetime
 import airflow
 from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 
-
-# In[12]:
-
-
 args = {
     'owner': 'Airflow',
-    'start_date': airflow.utils.dates.days_ago(2),
+    'start_date': datetime(2020,1,1),
 }
 
 dag = DAG(
     dag_id='test_dag',
     default_args=args,
-    schedule_interval= None,
+    schedule_interval='@daily',
     dagrun_timeout=timedelta(minutes=60),
 )
-
-
-# In[16]:
-
 
 task1 = DummyOperator(
     task_id='task1',
@@ -43,20 +29,15 @@ task3 = DummyOperator(
     task_id='task3',
     dag=dag,
 )
+
 task4 = DummyOperator(
     task_id='task4',
     dag=dag,
-) 
+)   
+    
 task5 = DummyOperator(
     task_id='task5',
     dag=dag,
 )
 task1 >> task2 >> task3 >> task5
 task2 >> task4 >> task5
-
-
-# In[ ]:
-
-
-
-
