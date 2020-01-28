0dbe11f0931c3273fa7238c00801d9087b2b64d0
jbastiaansen1 <60383797+jbastiaansen1@users.noreply.github.com>
2020-01-28 11:41:31 +0100

    Add files via upload

---
+++ dags/test_dag (2).py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+from datetime import timedelta
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+
+# In[18]:
+
+
+args = {
+    'owner': 'Airflow',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+dag = DAG(
+    dag_id='test_dag',
+    default_args=args,
+    schedule_interval='0 0 * * *',
+    dagrun_timeout=timedelta(minutes=60),
+)
+
+
+# In[23]:
+
+
+task1 = DummyOperator(
+    task_id='task1',
+    dag=dag,
+)
+task2 = DummyOperator(
+    task_id='task2',
+    dag=dag,
+)
+task3 = DummyOperator(
+    task_id='task3',
+    dag=dag,
+)
+
+task4 = DummyOperator(
+    task_id='task4',
+    dag=dag,
+)   
+    
+task5 = DummyOperator(
+    task_id='task5',
+    dag=dag,
+)
+task1 >> task2 >> task3 >> task5
+task2 >> task4 >> task5
+
+
+# In[ ]:
+
+
+
+
